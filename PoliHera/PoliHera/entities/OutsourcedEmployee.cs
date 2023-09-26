ab81e7a478e506f703d266b4404cf8609497d255
Obatista02 <guilherme.santos201412@gmail.com>
2023-09-25 21:57:17 -0300

    POO

---
+++ PoliHera/PoliHera/entities/OutsourcedEmployee.cs
@@ -0,0 +1,19 @@
+﻿namespace PoliHera.entities
+{
+   class OutsourcedEmployee : Employee
+    {
+
+        public double AdditionalCharge { get; set; }
+
+
+    public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) :base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+
+       
+    }
+}
