4cbd1b3c08c028f550479d08fdcd6e2326587675
Obatista02 <guilherme.santos201412@gmail.com>
2023-09-25 21:58:44 -0300

    POO

---
+++ Herança/Herança/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Herança.entities;
+
+Account aac1 = new Account(1001,"Alex", 500);
+Account aac2 = new SavingAccount(1002, "Anna", 500,0.01);
+
+
+aac1.WithDraw(10);
+aac2.WithDraw(10);
+
+Console.WriteLine(aac1.Balance);Console.WriteLine(aac2.Balance);
---
+++ Herança/Herança/entities/SavingAccount.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Herança.entities
+{
+         sealed class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccount() { }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate) : base (number, holder, balance)
+        {  
+            InterestRate = interestRate; 
+        }
+
+
+        public override void WithDraw(double amount)
+        {
+            Balance -=amount;
+        }
+        public void UpdateBalance(double amount)
+        {
+            Balance += amount * InterestRate;
+        }
+    }
+}
