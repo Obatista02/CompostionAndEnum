ec227bf68e81cde8d594d70ec031460436e95ca0
Obatista02 <guilherme.santos201412@gmail.com>
2023-10-05 21:10:00 -0300

    fex arquivos

---
+++ Arquivos/fex/ParaExercicios/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
+
+string path = @"C:\Users\Guilherme Batista\OneDrive\Documentos\arquivos";
+
+
+try
+{
+
+    {
+       var folder = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+        Console.WriteLine("FOLDERS: ");
+
+        foreach (string folderItem in folder)
+        {
+            Console.WriteLine(folderItem);
+        }
+        Console.WriteLine();
+        
+        var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+        Console.WriteLine("Files: ");
+
+        foreach (string folderItem in files)
+        {
+            Console.WriteLine(folderItem);
+        }
+
+
+        Console.WriteLine();
+
+        Directory.CreateDirectory(path + "\\newFolder");
+    }
+
+}catch(IOException e)
+{
+    Console.WriteLine(e.Message);
+}
