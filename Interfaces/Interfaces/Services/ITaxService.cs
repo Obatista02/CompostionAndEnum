4a85be90854df2c236987445555223f0bcaca252
Obatista02 <guilherme.santos201412@gmail.com>
2023-10-06 22:47:13 -0300

    Interface/services

---
+++ Interfaces/Interfaces/Services/ITaxService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Interfaces.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
