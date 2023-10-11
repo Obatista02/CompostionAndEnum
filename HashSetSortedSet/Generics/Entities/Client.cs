64daf5dfa349e11fc2baa67a371da0cb5c773a0c
Obatista02 <guilherme.santos201412@gmail.com>
2023-10-11 20:39:54 -0300

    Conjunto HashSet e SortedSet

---
+++ HashSetSortedSet/Generics/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Generics.Entities
+{
+    internal class Client
+    {
+        public String Name { get; set; }
+        public string Email { get; set; }
+
+
+        public override bool Equals(object obj)
+        {   if(!(obj is Client))
+            {
+                return false;
+            }
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ HashSetSortedSet/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+SortedSet<int> a = new SortedSet<int>(){ 0,2,3,4,5,6, 9,10};
+SortedSet<int> b = new SortedSet<int>() { 0, 22, 33, 514, 125, 62, 19, 10 };
+
+//uniao em forma ordenada e sem repetir
+
+SortedSet<int> c = new SortedSet<int>(a);
+c.UnionWith(b);
+PrintCollection(c);
+
+//intersection, separa os numeros que se repetem
+
+SortedSet<int> d = new SortedSet<int>(a);
+d.IntersectWith(b);
+PrintCollection(d);
+
+//diferrence
+
+SortedSet<int> e = new SortedSet<int>(a);
+e.ExceptWith(b);
+PrintCollection(e);
+
+
+
+static void PrintCollection<Generic>(IEnumerable<Generic> collection)
+{
+    foreach (Generic item in collection)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+
+}
