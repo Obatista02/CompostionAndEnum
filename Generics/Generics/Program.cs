4449d9867ee8cb1c10615852c5bd3b308b037b1d
Obatista02 <guilherme.santos201412@gmail.com>
2023-10-10 22:11:25 -0300

    Tipo Genericos

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using Generics;
+
+PrintService<string> printService = new PrintService<string>();
+
+Console.Write("how many values? ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string x = Console.ReadLine();
+
+    printService.AddValue(x);
+
+}
+printService.Print();
+Console.WriteLine("First: "+ printService.First());
