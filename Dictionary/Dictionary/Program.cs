d7c6a237ecf0c5305b9a3a3b467f7207e6693a39
Obatista02 <guilherme.santos201412@gmail.com>
2023-10-16 22:11:43 -0300

    Valor e chave

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+try { 
+cookies["User"] = "Maria";
+cookies["email"] = "Maria@gmail.com";
+cookies["phone"] = "997722558";
+cookies["phone"] = "911111111";
+
+Console.WriteLine(cookies["phone"]);
+Console.WriteLine(cookies["email"]);
+
+cookies.Remove("email");
+
+
+    Console.WriteLine("Size: " + cookies.Count);
+    Console.WriteLine("ALL Cookies: ");
+    foreach (KeyValuePair<string, string> item in cookies)
+    {
+        Console.WriteLine(item.Key + ": "+ item.Value);
+    }
+
+}
+catch( KeyNotFoundException e)
+{
+    Console.WriteLine("There is not key");
+}
