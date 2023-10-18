06c49375ce1189f663069f749607a79539677ead
Obatista02 <guilherme.santos201412@gmail.com>
2023-10-17 21:50:16 -0300

    Expressao lambda com Comparison

---
+++ CompareTo/Compare/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Nelio.entities;
+
+List<Product> list = new List<Product>();
+
+list.Add(new Product("TV", 900));
+list.Add(new Product("Notebook", 1200));
+list.Add(new Product("Tablet", 450));
+
+//Expressao Lambda
+//pode colocar a expressao direta no metodo Sort
+//Comparison<Product> comp = (p1,p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+list.Sort(CompareProducts);
+
+foreach (Product item in list)
+{
+    Console.WriteLine(item);
+}
+
+static int CompareProducts(Product p1, Product p2)
+{
+    return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+}
